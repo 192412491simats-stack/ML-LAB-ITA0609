6a49f7affbf275ca24c2c4d91cbf0bcb1dc1e938
192412491simats-stack <192412491.simats@saveetha.com>
2026-07-24 12:54:26 +0530

    Add files via upload

---
+++ rnn_debug.py
@@ -0,0 +1,15 @@
+# Buggy Code
+
+print("RNN Debugging Example")
+
+sequence = [10, 20, 30, 40, 50]
+
+total = 0
+
+# Bug: Wrong variable name
+for value in sequences:
+    total += value
+
+average = total / len(sequence)
+
+print(average)
